2f7b33d178efe104c75e080dd51d328bb7e49ffa
MahaCreative <gunturmadjid.3@gmail.com>
2024-07-09 14:40:38 +0800

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,3 +32,7 @@ window.Echo = new Echo({
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
     enabledTransports: ["ws", "wss"],
 });
+window.Echo.channel("trafics").listen("TakePicture", (data) => {
+    captureImage();
+    console.log(data);
+});
